f28cbc7aabb3ed5a169065859ad7b82a8abaeaee
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2025-12-25 20:47:10 +0500

    Task0

---
+++ Tyuiu.StarikovaVYA.Sprint1.Task0.V11.Test/DataServiseTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.StarikovaVYA.Sprint1.Task0.V11.Lib;
+
+namespace Tyuiu.StarikovaVYA.Sprint1.Task0.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(7, res);
+        }
+    }
+}
---
+++ Tyuiu.StarikovaVYA.Sprint1.Task0.V11/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.StarikovaVYA.Sprint1.Task0.V11.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Старикова В. Я. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнила: Старикова Варвара Яковлевна | СМАРТб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение 4*5/2-18/2/3 и          *");
+        Console.WriteLine("* печатает результат на экране.                                           *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 4*5/2-18/2/3                                                            *");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine(ds.Calculate());
+        Console.ReadLine();
+    }
+}
