645b26391d27e3ce4dfb082858fd16437d355d25
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2026-01-18 22:16:53 +0500

    Task1

---
+++ Tyuiu.StarikovaVYA.Sprint1.Task1.V24.Test/DataServiseTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.StarikovaVYA.Sprint1.Task1.V24.Lib;
+
+namespace Tyuiu.StarikovaVYA.Sprint1.Task1.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 3.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
---
+++ Tyuiu.StarikovaVYA.Sprint1.Task1.V24/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.StarikovaVYA.Sprint1.Task1.V24.Lib;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнила: Старикова В. Я. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнила: Старикова Варвара Яковлевна | СМАРТб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле (1-x)/(2+y) и печатает его на экране.    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x, y;
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x, y));
+
+        Console.ReadKey();
+    }
+}
