a05d5eadb044889c45a5ee1d2c324b8a2165076f
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2026-01-21 19:44:26 +0500

    Task6

---
+++ Tyuiu.StarikovaVYA.Sprint1.Task6.V9.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.StarikovaVYA.Sprint1.Task6.V9.Lib;
+namespace Tyuiu.StarikovaVYA.Sprint1.Task6.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var x = "всем привет";
+            var res = ds.MoveLetterToStart(x);
+            var wait = "мвсе тприве";
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.StarikovaVYA.Sprint1.Task6.V9/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.StarikovaVYA.Sprint1.Task6.V9.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнила: Аристова А. К. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнила: Аристова Алина Константиновна | СМАРТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*  Написать программу: пользователь вводит текст. Напечатать все слова,   *");
+        Console.WriteLine("* перенеся их последнюю букву в начало.                                   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите строку:");
+        string x = Console.ReadLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.MoveLetterToStart(x));
+
+        Console.ReadKey();
+    }
+}
