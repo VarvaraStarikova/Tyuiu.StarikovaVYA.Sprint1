0677b9ca8ba7e93a2df9906353e44bdccc0f9af4
VarvaraStarikova <Tyuiu.StarikovaVYA@outlook.com>
2026-01-21 19:45:45 +0500

    Task6

--- Tyuiu.StarikovaVYA.Sprint1.Task6.V9/Program.cs
+++ Tyuiu.StarikovaVYA.Sprint1.Task6.V9/Program.cs
@@ -4,16 +4,16 @@ internal class Program
     private static void Main(string[] args)
     {
         DataService ds = new DataService();
-        Console.Title = "Спринт #1 | Выполнила: Аристова А. К. | СМАРТб-25-1";
+        Console.Title = "Спринт #1 | Выполнила: Старикова В. Я. | СМАРТб-25-1";
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #1                                                               *");
         Console.WriteLine("* Тема: Работа со строками класс String                                   *");
         Console.WriteLine("* Задание #6                                                              *");
-        Console.WriteLine("* Вариант #5                                                              *");
-        Console.WriteLine("* Выполнила: Аристова Алина Константиновна | СМАРТб-25-1                  *");
+        Console.WriteLine("* Вариант #9                                                              *");
+        Console.WriteLine("* Выполнила: Старикова Варвара Яковлевна | СМАРТб-25-1                    *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
-        Console.WriteLine("*  Написать программу: пользователь вводит текст. Напечатать все слова,   *");
+        Console.WriteLine("* Написать программу: пользователь вводит текст. Напечатать все слова,    *");
         Console.WriteLine("* перенеся их последнюю букву в начало.                                   *");
         Console.WriteLine("*                                                                         *");
         Console.WriteLine("***************************************************************************");
